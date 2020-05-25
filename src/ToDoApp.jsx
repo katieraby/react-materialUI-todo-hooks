0275fd19583dde5764d80b577bd3a71ceaa0d146
Katie <50029859+katieraby@users.noreply.github.com>
2020-05-25 16:04:47 +0100

    Added custom useInputState hook, created ToDoForm and ToDoList components from ToDoApp

--- src/ToDoApp.jsx
+++ src/ToDoApp.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+import ToDoForm from "./ToDoForm";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
 
 function ToDoApp() {
-  return <h1>Todos</h1>;
+  const initialTodos = [
+    { id: 1, task: "Clean Fishtank", completed: false },
+    { id: 2, task: "Wash Car", completed: true },
+    { id: 3, task: "Wash Hair", completed: false },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+  const addTodo = (newTodoText) => {
+    setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+  };
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOs with hooks</Typography>
+        </Toolbar>
+      </AppBar>
+      <ToDoForm addTodo={addTodo} />
+      <ToDoList todos={todos} />
+    </Paper>
+  );
 }
 
 export default ToDoApp;
+
+// - ToDoApp
+//   -ToDoForm
+//   -ToDoList
+//     -ToDoItem
+
+// id, task, completed
